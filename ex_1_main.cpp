17759344cc0704fd433c0eaefd494c520d41b15d
0 <0>
2020-11-24 12:30:28 +0900

    numが2からn-1の数で割れるか計算できるように修正し、nth_primeを修正した

--- ex_1_main.cpp
+++ ex_1_main.cpp
@@ -22,20 +22,23 @@ bool is_prime(int num) {
 
 int nth_prime(unsigned int a, unsigned int d, unsigned int n)
 {
-	int num = a;
-	while (num <= CPP2_PRIME_UPPER_LIMIT) {
-		std::cout << num << std::endl;
-		num += d;
+	int current = a;
+	int count = 0;
+	while (current < CPP2_PRIME_UPPER_LIMIT) {
+		if (is_prime(current)) {
+			count++;
+			if (count == n) {
+				return current;
+			}
+		}
+		current += d;
 	}
 	return(-1);
 };
 
 int main()
 {
-	std::cout << is_prime(4) << std::endl;
-	std::cout << is_prime(5) << std::endl;
-	std::cout << is_prime(6) << std::endl;
-	std::cout << is_prime(7) << std::endl;
+	std::cout << nth_prime(367, 186, 151) << std::endl;
 	// 以下、同様に、入出力例通りになるか確認せよ。
 	return 0;
 }
